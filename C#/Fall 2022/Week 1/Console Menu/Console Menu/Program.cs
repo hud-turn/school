4bdf68697266611a6e3ee08acf142ad592759fb5
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2022-08-25 11:52:45 -0600

    Added Console Menu

---
+++ C#/Fall 2022/Week 1/Console Menu/Console Menu/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace ConsoleMenu 
+{
+    class Program
+    {
+        static void Main()
+        {
+            int intuserinput = 0;
+            while (intuserinput != 2)
+            {
+                Console.Clear();
+                Console.WriteLine("Please select one of the following:\n1. About this developer\n2. Exit the program");
+                string struserinput = Console.ReadLine();
+                intuserinput = int.Parse(struserinput);
+                if (intuserinput == 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Hello, you have selected about this developer.\nThe developer of this program is Hudson who is a senior CIS major at UNC.");
+                    Console.ReadKey();
+                }
+                else if (intuserinput > 2 || intuserinput < 1) 
+                {
+                    Console.Clear();
+                    Console.WriteLine("Please enter 1 or 2");
+                    Console.ReadKey();
+                }
+            }
+
+        }
+
+    }
+}
