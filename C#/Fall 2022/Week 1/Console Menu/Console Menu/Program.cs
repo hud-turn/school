47e14a8ac9fdec371402a3457a3c7b988cdc1a56
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2022-08-25 11:54:55 -0600

    Accidentally Broke the Program

--- C#/Fall 2022/Week 1/Console Menu/Console Menu/Program.cs
+++ C#/Fall 2022/Week 1/Console Menu/Console Menu/Program.cs
@@ -8,10 +8,10 @@ namespace ConsoleMenu
             int intuserinput = 0;
             while (intuserinput != 2)
             {
-                string struserinput = Console.ReadLine();
-                intuserinput = int.Parse(struserinput);
                 Console.Clear();
                 Console.WriteLine("Please select one of the following:\n1. About this developer\n2. Exit the program");
+                string struserinput = Console.ReadLine();
+                intuserinput = int.Parse(struserinput);
                 if (intuserinput == 1)
                 {
                     Console.Clear();
