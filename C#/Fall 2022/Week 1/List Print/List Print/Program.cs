04879b2640e86c2e37ed9b6e136114948bf74133
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2022-08-25 12:13:50 -0600

    Adding Programs

---
+++ C#/Fall 2022/Week 1/List Print/List Print/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListPrint
+{
+    class ListPrint
+    {
+        static void Main(string[] args)
+        {
+            List<string> strlst = new List<string> {};//creating the empty list
+            strlst.Add("Mike");
+            strlst.Add("Rosa");
+            strlst.Add("Cal");
+            strlst.Add("Janice");
+            strlst.Add("Mandy");
+            strlst.Add("John");
+            strlst.Add("Steve");
+            strlst.Add("Fiona");
+            strlst.Add("Tanner");
+            strlst.Add("Brennan");
+            strlst.Sort();
+        }
+    }
+}
