d4d9e38c2f1ed1a52c030b6f34e09a1dea94e388
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2022-08-25 11:58:32 -0600

    Minor updates

--- C#/Fall 2022/Week 1/Console Menu/Console Menu/Program.cs
+++ C#/Fall 2022/Week 1/Console Menu/Console Menu/Program.cs
@@ -8,21 +8,21 @@ namespace ConsoleMenu
             int intuserinput = 0;
             while (intuserinput != 2)
             {
-                Console.Clear();
-                Console.WriteLine("Please select one of the following:\n1. About this developer\n2. Exit the program");
-                string struserinput = Console.ReadLine();
-                intuserinput = int.Parse(struserinput);
-                if (intuserinput == 1)
+                Console.Clear();//clears the terminal to make the window cleaner
+                Console.WriteLine("Please select one of the following:\n1. About this developer\n2. Exit the program");//main menu tells the user how to use the program
+                string struserinput = Console.ReadLine();//gets user input
+                intuserinput = int.Parse(struserinput);//converts user input string to int
+                if (intuserinput == 1)//looks to see if the user int is a 1
                 {
-                    Console.Clear();
-                    Console.WriteLine("Hello, you have selected about this developer.\nThe developer of this program is Hudson who is a senior CIS major at UNC.");
-                    Console.ReadKey();
+                    Console.Clear();//clears the terminal
+                    Console.WriteLine("Hello, you have selected about this developer.\nThe developer of this program is Hudson who is a senior CIS major at UNC.");//information
+                    Console.ReadKey();//exits information window when the user hits a key
                 }
-                else if (intuserinput > 2 || intuserinput < 1) 
+                else if (intuserinput > 2 || intuserinput < 1) //looks to see if the user int is greater than one or two
                 {
-                    Console.Clear();
-                    Console.WriteLine("Please enter 1 or 2");
-                    Console.ReadKey();
+                    Console.Clear();//clears terminal
+                    Console.WriteLine("Please enter 1 or 2");//tells user to enter 1 or 2
+                    Console.ReadKey();//exits this menu when user hits key
                 }
             }
 
