7c148101839dc88d880e30382a07115db5f8e1b9
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2022-08-29 00:41:57 -0600

    Making minor changes to debug code

--- C#/Fall 2022/Week 1/Letters and Numbers/Program.cs
+++ C#/Fall 2022/Week 1/Letters and Numbers/Program.cs
@@ -13,22 +13,21 @@ namespace lettersandnumbers
             int inttotal = 0, iq = 0, test = 0;
             string[] stringarray = new string[str.Length];//creating an array that we will output later
             char[] letters = { 'a', 'b', 'c', 'd','e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p','q','r','s','t','u','v','w','x','y','z'};
-            int[] numbers = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4781, 6765, 10946, 17711, 28657, 46368, 75025};
+            int[] numbers = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025};
             for (int inte = 0; inte < str.Length; inte++)//moving the string into the array
             {
                 stringarray[inte] = Char.ToString(str[inte]);//moving the string into an array so that we can reverse the string
             }
-            Console.ReadKey();
+            //Console.ReadKey();
             while (inttest != test)
             {
-                //Console.WriteLine("Letters: " + letters[iq]);
-                //Console.WriteLine("Numbers" + numbers[iq]);
-                if (iq < 25 && stringarray[test].Contains(letters[iq]))
+                if (iq < 26 && stringarray[test].Contains(letters[iq]))
                 {
+                    //Console.WriteLine("Letters: " + letters[iq] + "| " + numbers[iq]);
                     inttotal += numbers[iq];
                     test++;
                 }
-                if (iq == 25)
+                if (iq == 26)
                 {
                     iq = -1;
                 }
@@ -36,7 +35,7 @@ namespace lettersandnumbers
                 //Console.WriteLine("Inttotal" + inttotal);
                 //Console.ReadKey();
             }
-            Console.WriteLine(inttotal);
+            Console.WriteLine("The Fibonacci Number for this word is: " + inttotal);
             Console.ReadKey();
          }   
     }
