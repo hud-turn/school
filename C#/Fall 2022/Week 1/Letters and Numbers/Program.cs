15e215f871fc327251107d181d852249aaddcdae
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2022-08-29 00:45:32 -0600

    Making Minor Changes

--- C#/Fall 2022/Week 1/Letters and Numbers/Program.cs
+++ C#/Fall 2022/Week 1/Letters and Numbers/Program.cs
@@ -16,7 +16,7 @@ namespace lettersandnumbers
             int[] numbers = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025};
             for (int inte = 0; inte < str.Length; inte++)//moving the string into the array
             {
-                stringarray[inte] = Char.ToString(str[inte]);//moving the string into an array so that we can reverse the string
+                stringarray[inte] = Char.ToString(str[inte]);//moving the string into an array so that we can compare it
             }
             //Console.ReadKey();
             while (inttest != test)
