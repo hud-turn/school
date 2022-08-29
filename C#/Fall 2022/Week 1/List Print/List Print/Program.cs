c19db6da7a032802124edfbbd3704065e232b154
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2022-08-28 19:27:19 -0600

    Making Changes

--- C#/Fall 2022/Week 1/List Print/List Print/Program.cs
+++ C#/Fall 2022/Week 1/List Print/List Print/Program.cs
@@ -7,10 +7,11 @@ namespace ListPrint
     {
         static void Main(string[] args)
         {
+            int inti = 9;//starting at 9 and counting down
             List<string> strlst = new List<string> {};//creating the empty list
             strlst.Add("Mike");
             strlst.Add("Rosa");
-            strlst.Add("Cal");
+            strlst.Add("Cal");//adding names to the list
             strlst.Add("Janice");
             strlst.Add("Mandy");
             strlst.Add("John");
@@ -18,7 +19,13 @@ namespace ListPrint
             strlst.Add("Fiona");
             strlst.Add("Tanner");
             strlst.Add("Brennan");
-            strlst.Sort();
+            strlst.Sort();//sorting the list alphabetically
+            while (inti != -1)//we need to read the space in 0 on the list
+            {
+                Console.WriteLine(strlst[inti]);//outputting the contents of that specific space in the list
+                inti--;//subtracting 1 from inti
+            }
+            Console.ReadKey();//preventing the program from closing
         }
     }
 }
