420e98e67a69c2ef4d0818668ff37860eff1df00
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2022-08-30 12:07:25 -0600

    Changes

--- C#/Fall 2022/Week 1/Analytics/Analytics/Program.cs
+++ C#/Fall 2022/Week 1/Analytics/Analytics/Program.cs
@@ -13,14 +13,11 @@ namespace ListPrint
             Console.WriteLine("Please input a name you would like to remove from the list above: ");//telling the user what to input
             for (int i = 0; i != declst.Count(); i++)
             {
+
                decsum += declst[i];
             }
             Console.WriteLine(decsum);
-            //Console.WriteLine("We were able to remove " + userinput + " from the list.\n\nThe list is now:");//tells the word that was removed
-           // foreach (string str in strlst)
-            //{
-                //Console.WriteLine(str);//outputs the list of remaining names
-            //}
+            
             Console.ReadKey();//preventing the program from closing
         }
     }
