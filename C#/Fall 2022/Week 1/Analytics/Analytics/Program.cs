824d80d84dd734f44108c746e3d6aa312bd30d68
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2022-08-30 12:06:33 -0600

    Changes

--- C#/Fall 2022/Week 1/Analytics/Analytics/Program.cs
+++ C#/Fall 2022/Week 1/Analytics/Analytics/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;//importing the library that allows us to use list
+
+namespace ListPrint
+{
+    class ListPrint
+    {
+        static void Main(string[] args)
+        {
+            List<decimal> declst = new List<decimal> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, };//creating the empty list
+            decimal decsum = 0;
+            //int inti = 9;//starting at 9 and counting down
+            Console.WriteLine("Please input a name you would like to remove from the list above: ");//telling the user what to input
+            for (int i = 0; i != declst.Count(); i++)
+            {
+               decsum += declst[i];
+            }
+            Console.WriteLine(decsum);
+            //Console.WriteLine("We were able to remove " + userinput + " from the list.\n\nThe list is now:");//tells the word that was removed
+           // foreach (string str in strlst)
+            //{
+                //Console.WriteLine(str);//outputs the list of remaining names
+            //}
+            Console.ReadKey();//preventing the program from closing
+        }
+    }
+}
