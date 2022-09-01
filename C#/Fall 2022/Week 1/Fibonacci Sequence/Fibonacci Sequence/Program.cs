92eeb4fc51602cba1f1d49d07eca4908ec7af92b
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2022-09-01 12:17:09 -0600

    Attempting to clean up code

---
+++ C#/Fall 2022/Week 1/Fibonacci Sequence/Fibonacci Sequence/Program.cs
@@ -0,0 +1,12 @@
+﻿List<int> numbers = new List<int> { };
+for (int intfibone = 0, intfiblst = 0; intfiblst != 26; intfiblst++)//creating the fibonacci sequence
+{
+    numbers.Add(intfibone);
+
+    if (intfiblst == 1)
+    {
+        intfibone++;
+    }
+
+    Console.WriteLine(intfibone);
+}
--- C#/Fall 2022/Week 1/Letters and Numbers/Program.cs
+++ C#/Fall 2022/Week 1/Letters and Numbers/Program.cs
@@ -12,12 +12,24 @@ namespace lettersandnumbers
             string str = strinput.ToLower();//converts the string to uppercase so that the program will accept the input
             int inttest = strinput.Length, inttotal = 0, iq = 0, test = 0;//setting parametrs for the program
 
-            List<string> stringlist = new List<string> { };//creating a list that we load the user input into
+            List<string> stringlist = new List<string> {};//creating a list that we load the user input into
             List<char> letters = new List<char> {};//creating the alphabet and also allowing for spaces in the word
-            List <int> numbers = new List<int> { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025};//creating the list of fibonnaci numbers
-            for (char c = 'a'; c <= 'z'; c++)
+            List <int> numbers = new List<int> {};//creating the list of fibonnaci numbers
+            for (char c = 'a'; c <= 'z'; c++)//creating the alphabet
             {
                 letters.Add(c);//do something with letter 
+            }
+            for (int intfibone = 0, intfiblst = 0; intfiblst != 26; intfiblst++)//creating the fibonacci sequence
+            {
+                numbers.Add(intfibone);
+                if(intfiblst == 1)
+                {
+                    intfibone = intfiblst - 1;
+                }
+                if (intfiblst > 1)
+                {
+                    intfibone = (intfiblst - 1) + numbers[intfiblst - 2];
+                }
             }
 
             for (int inte = 0; inte < str.Length; inte++)//moving the string into the array
