f9ca3c35ede75ee6b20253287d2e20bf41dce09a
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2022-09-06 11:22:54 -0600

    Creating the overarching structure

--- C#/Fall 2022/Week 1/Student Grades Manager 1 (without OOP Concepts)/Program.cs
+++ C#/Fall 2022/Week 1/Student Grades Manager 1 (without OOP Concepts)/Program.cs
@@ -1,1 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;//importing the library that allows us to use list
+
+namespace ListPrint
+{
+    class ListPrint
+    {
+        static void Main(string[] args)
+        {
+            List<string> strnamelst = new List<string> { };//creating the empty list
+            List<int> intamelst = new List<int> { };//creating the empty list
+            int intuserinput = 0;
+            while (intuserinput != 3)
+            {
+                
+                Console.WriteLine("Please select one of the following commands to issue:");
+                Console.WriteLine("1. Enter Students (name, grade)\n2. Display student grade average\n3. Quit");
+                intuserinput = int.Parse(Console.ReadLine());
+                if (intuserinput == 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("1");
+                    Console.ReadKey();
+                }
+                else if (intuserinput == 2)
+                {
+                    Console.Clear();
+                    Console.WriteLine("2");
+                    Console.ReadKey();
+                }
+                else if (intuserinput == 3)
+                {
+                    Console.Clear();
+                    Console.WriteLine("3");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Else");
+                    Console.ReadKey();
+                }
+                Console.Clear();
+            }
+        }
+    }
+}
