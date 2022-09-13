82ee94b52cfab33f3a804ebfe1932e19823268df
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2022-09-13 12:09:27 -0600

    OOP

---
+++ C#/Fall 2022/Week 2/Exercise 1/Exercise 1/Grade.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace In_Class_Work
+{
+    internal class Student          
+    {
+        public Student (string firstname, string lastname, double grade)//don't do uppercase parameters also public allows the for that program to be global
+        {
+            this.FirstName = firstname;//this passes firstname from our program.cs file over to 
+            this.LastName = lastname;
+            this.Grade = grade;
+        }
+        public string FirstName { get; set; }//get means I can read and set means I can change the value of that property
+        public string LastName { get; set; }
+        public double Grade { get; set; } //get means that I can read the value of that property and set means that I can change the value
+    }
+}
---
+++ C#/Fall 2022/Week 2/Exercise 1/Exercise 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using In_Class_Work;
+using System;
+namespace In_Class_Work
+{
+    class Program
+    {
+        static void Main()
+        {
+            double doubgrade = 0;
+            Student mySelf = new Student("Moe ","Manshad", 10);//myself is the variable and Student is the object
+            Student S0 = new Student("S0 ", "S0", 10);
+            Student S1 = new Student("S1 ", "S1", 10);
+            //Console.WriteLine(mySelf.FirstName+ mySelf.LastName + mySelf.Grade);
+            List<Student> strnamelst = new List<Student> {mySelf, S0, S1 };//creating the list and loading mySelf, S0, and S1 into the list.
+            foreach (Student s in strnamelst)
+            {
+                Console.WriteLine(s.FirstName + s.LastName + s.Grade);
+                doubgrade += s.Grade;
+            }
+            Console.WriteLine(doubgrade/3);
+        }
+    }
+}
