fae6e0314c0cfd8a7bbc6b8e2265674e6ddbc1bc
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2022-09-15 10:55:05 -0600

    Updating Code

--- C#/Fall 2022/Week 2/Exercise 1/Exercise 1/Program.cs
+++ C#/Fall 2022/Week 2/Exercise 1/Exercise 1/Program.cs
@@ -11,13 +11,14 @@ namespace In_Class_Work
             Student S0 = new Student("S0 ", "S0", 10);
             Student S1 = new Student("S1 ", "S1", 10);
             //Console.WriteLine(mySelf.FirstName+ mySelf.LastName + mySelf.Grade);
-            List<Student> strnamelst = new List<Student> {mySelf, S0, S1 };//creating the list and loading mySelf, S0, and S1 into the list.
-            foreach (Student s in strnamelst)
+            List<Student> strnamelst = new List<Student> {mySelf, S0, S1 };//creating the list and loading objects mySelf, S0, and S1 into the list.
+            foreach (Student s in strnamelst)//iterating through every line in strnamelst
             {
-                Console.WriteLine(s.FirstName + s.LastName + s.Grade);
+                Console.WriteLine(s.FirstName + s.LastName + s.Grade);//printing the output
                 doubgrade += s.Grade;
             }
             Console.WriteLine(doubgrade/3);
+            Console.ReadKey();
         }
     }
 }
