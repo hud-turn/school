d6d11c169b628871c032494abbf6c60909a54b7d
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2022-09-20 11:17:53 -0600

    Changes

--- C#/Fall 2022/Week 2/OOP Exercise Objects and Classes/GetAverageGrade.cs
+++ C#/Fall 2022/Week 2/OOP Exercise Objects and Classes/GetAverageGrade.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using In_Class_Work;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,20 @@ using System.Threading.Tasks;
 
 namespace OOP_Exercise_Objects_and_Classes
 {
-    internal class Class1
+    internal class GetAverageGrade
     {
+        public GetAverageGrade(decimal decgrade)
+        {
+            this.DecGrade = decgrade;
+        }
+        public decimal DecGrade { get; set; }
+        public decimal ()
+        {
+            foreach (Student s in strnamelst)//iterating through every line in strnamelst
+            {
+                Console.WriteLine(s.FirstName + s.LastName + s.Grade);//printing the output
+                doubgrade += s.Grade;
+            }
+        }
     }
 }
--- C#/Fall 2022/Week 2/OOP Exercise Objects and Classes/Program.cs
+++ C#/Fall 2022/Week 2/OOP Exercise Objects and Classes/Program.cs
@@ -7,11 +7,18 @@ namespace In_Class_Work
         static void Main()
         {
             double doubgrade = 0;
-            Student mySelf = new Student("Moe ", "Manshad", 10);//myself is the variable and Student is the object
-            Student S0 = new Student("S0 ", "S0", 10);
-            Student S1 = new Student("S1 ", "S1", 10);
+            Student S0 = new Student("Moe ", "Manshad", 10);//myself is the variable and Student is the object
+            Student S1 = new Student("S0 ", "S0", 20);
+            Student S2 = new Student("S2 ", "S1", 30);
+            Student S3 = new Student("S3 ", "S1", 40);
+            Student S4 = new Student("S4 ", "S1", 50);
+            Student S5 = new Student("S5 ", "S1", 60);
+            Student S6 = new Student("S6 ", "S1", 70);
+            Student S7 = new Student("S7 ", "S1", 80);
+            Student S8 = new Student("S8 ", "S1", 90);
+            Student S9 = new Student("S9 ", "S1", 100);
             //Console.WriteLine(mySelf.FirstName+ mySelf.LastName + mySelf.Grade);
-            List<Student> strnamelst = new List<Student> { mySelf, S0, S1 };//creating the list and loading objects mySelf, S0, and S1 into the list.
+            List<Student> strnamelst = new List<Student> {S0, S1, S2, S3, S4, S5, S6, S7, S8, S9 };//creating the list and loading objects mySelf, S0, and S1 into the list.
             foreach (Student s in strnamelst)//iterating through every line in strnamelst
             {
                 Console.WriteLine(s.FirstName + s.LastName + s.Grade);//printing the output
