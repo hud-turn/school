4d44e36e036e67d94f62fba84fbfd03862559b38
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2022-09-20 11:02:13 -0600

    Changes

---
+++ C#/Fall 2022/Week 2/OOP Exercise Objects and Classes/GetAverageGrade.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Exercise_Objects_and_Classes
+{
+    internal class Class1
+    {
+    }
+}
