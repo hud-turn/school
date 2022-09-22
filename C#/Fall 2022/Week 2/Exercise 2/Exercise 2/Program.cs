c91ffe24628eb2e8dc732aef7c3fe292b2ceb3da
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2022-09-22 17:56:18 -0600

    Update

--- C#/Fall 2022/Week 2/Exercise 2/Exercise 2/Program.cs
+++ C#/Fall 2022/Week 2/Exercise 2/Exercise 2/Program.cs
@@ -8,16 +8,21 @@ namespace In_Class_Work
     {
         static void Main()
         {
-            
-            GradedCourse x = new GradedCourse("Test", 80);
-            GradedCourse y = new GradedCourse("English", 30);
-            PassFailCourse cc = new PassFailCourse("Composition", true);
-            PassFailCourse dd = new PassFailCourse("Old English", false);
+            string comp_i = "Composition I", mod_eng = "Modern English", comp_ii = "Composition II", old_eng="Old English";
+            Console.WriteLine("An english degree has three courses which need to be passed in order to obtain a degree.\nThese courses are " + comp_i + ", " + mod_eng + ", " + comp_ii + ", and " + old_eng + ".");
+            GradedCourse x = new GradedCourse(comp_i, 80);//running test and 80 through the graded course. If it passes then x will be true
+            GradedCourse y = new GradedCourse(mod_eng, 30);
+            PassFailCourse aa = new PassFailCourse(comp_ii, true);
+            PassFailCourse bb = new PassFailCourse(old_eng, false);
 
-            //Console.WriteLine(y.Passed());
+            //Console.WriteLine("x" + x.Passed());
+            //Console.WriteLine("y" + y.Passed());
+            //Console.WriteLine("aa" + aa.Passed());
+            //Console.WriteLine("bb" + bb.Passed());
 
-            Degree d = new Degree(cc, dd, x, y);
-            Console.WriteLine(d.Passed());
+
+            Degree d = new Degree(aa, bb, x, y);
+            Console.WriteLine("\nHas " + d.Passed());
             Console.ReadKey();
         }
     }
