36acc8d6c7d9f1f10e9febefbdbfb44c748d5b79
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2022-09-22 19:31:22 -0600

    Update

--- C#/Fall 2022/Week 2/Exercise 2/Exercise 2/Program.cs
+++ C#/Fall 2022/Week 2/Exercise 2/Exercise 2/Program.cs
@@ -10,19 +10,26 @@ namespace In_Class_Work
         {
             string comp_i = "Composition I", mod_eng = "Modern English", comp_ii = "Composition II", old_eng="Old English";
             Console.WriteLine("An english degree has three courses which need to be passed in order to obtain a degree.\nThese courses are " + comp_i + ", " + mod_eng + ", " + comp_ii + ", and " + old_eng + ".");
-            GradedCourse x = new GradedCourse(comp_i, 80);//running test and 80 through the graded course. If it passes then x will be true
+            GradedCourse x = new GradedCourse(comp_i, 80);//running comp_i and a grade which is 80 through the graded course. If the grade passes then x will be true
             GradedCourse y = new GradedCourse(mod_eng, 30);
             PassFailCourse aa = new PassFailCourse(comp_ii, true);
-            PassFailCourse bb = new PassFailCourse(old_eng, false);
+            PassFailCourse bb = new PassFailCourse(old_eng, true);
 
             //Console.WriteLine("x" + x.Passed());
             //Console.WriteLine("y" + y.Passed());
             //Console.WriteLine("aa" + aa.Passed());
             //Console.WriteLine("bb" + bb.Passed());
 
-
             Degree d = new Degree(aa, bb, x, y);
-            Console.WriteLine("\nHas the student passed three classes and obtained their degree? s" + d.Passed());
+            Console.Write("\nHas the student passed three classes and obtained their degree? ");
+            if (d.Passed() == true)
+            {
+                Console.Write("Yes");
+            }
+            else
+            {
+                Console.Write("No");
+            }
             Console.ReadKey();
         }
     }
