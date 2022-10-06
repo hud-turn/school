24eb52de50324407b35da846f0849ec2654e28ad
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2022-10-06 11:21:34 -0600

    New Program

---
+++ C#/Fall 2022/Week 2/OOP Exercise5 Objects and Classes/Program.cs
@@ -0,0 +1,16 @@
+﻿using In_Class_Work;
+using System;
+namespace In_Class_Work
+{
+    class Program
+    {
+        static void Main()
+        {
+            int userinput = 0; 
+            while (userinput != 5)
+            {
+
+            }
+        }
+    }
+}
---
+++ C#/Fall 2022/Week 2/OOP Exercise5 Objects and Classes/Savings_Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Exercise5_Objects_and_Classes
+{
+    internal class Savings_Account
+    {
+        public Savings_Account()
+        {
+
+        }
+    }
+}
