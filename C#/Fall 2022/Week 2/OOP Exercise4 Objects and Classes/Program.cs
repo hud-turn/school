f1586e20fcadd5b637ebc03ba2a520486f1593eb
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2022-10-06 11:10:20 -0600

    Changes

--- C#/Fall 2022/Week 2/OOP Exercise4 Objects and Classes/Program.cs
+++ C#/Fall 2022/Week 2/OOP Exercise4 Objects and Classes/Program.cs
@@ -6,20 +6,20 @@ namespace In_Class_Work
     {
         static void Main()
         {
-            Players Coach = new Players("Coach", "Test1", 50);
-            Players Player1 = new Players("Quarterback", "Test2", 20);
-            Players Player2 = new Players("Runningback", "Test3", 22);
-            Players Player3 = new Players("Runningback", "Test4", 24);
-            Players Player4 = new Players("Wide Reciever", "Test5", 23);
-            Players Player5 = new Players("Tight End", "Test6", 25);
-            Players Player6 = new Players("Corner Backs", "Test7", 26);
-            Players Player7 = new Players("Corner Backs", "Test8", 27);
-            Players Player8 = new Players("Safeties", "Test9", 27);
-            Players Player9 = new Players("Safeties", "Test10", 27);
-            Players Player10 = new Players("Defensive Tackle", "Test11", 27);
-            Players Player11 = new Players("Defensive End", "Test12", 27);
-            Players Player12 = new Players("Line Backer", "Test13", 27);
-            Players Player13 = new Players("Kicker", "Test14", 27);
+            Players Coach = new Players("Coach            |", "Test1 ", 50);
+            Players Player1 = new Players("Quarterback      |", "Test2 ", 20);
+            Players Player2 = new Players("Runningback      |", "Test3 ", 22);
+            Players Player3 = new Players("Runningback      |", "Test4 ", 24);
+            Players Player4 = new Players("Wide Reciever    |", "Test5 ", 23);
+            Players Player5 = new Players("Tight End        |", "Test6 ", 25);
+            Players Player6 = new Players("Corner Backs     |", "Test7 ", 26);
+            Players Player7 = new Players("Corner Backs     |", "Test8 ", 27);
+            Players Player8 = new Players("Safeties         |", "Test9 ", 27);
+            Players Player9 = new Players("Safeties         |", "Test10", 27);
+            Players Player10 = new Players("Defensive Tackle |", "Test11", 27);
+            Players Player11 = new Players("Defensive End    |", "Test12", 27);
+            Players Player12 = new Players("Line Backer      |", "Test13", 27);
+            Players Player13 = new Players("Kicker           |", "Test14", 27);
 
             List<Players> listofplayers = new List<Players>();
             listofplayers.Add(Coach);
@@ -40,7 +40,7 @@ namespace In_Class_Work
 
             Team TeamOne = new Team("Broncos", "Denver", listofplayers);
 
-            Console.WriteLine(TeamOne.TeamName + TeamOne.CityName);
+            Console.WriteLine(TeamOne.TeamName + " " + TeamOne.CityName + "\n");
             foreach (Players s in TeamOne.LstPlayers)
             {
                 Console.WriteLine(s.PlayerPOS + " " + s.PlayerName  + " " + s.Age);
