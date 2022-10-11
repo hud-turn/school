ded530a5d1d35368f7eb6de1a9e3e5a06172fe78
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2022-10-11 11:19:44 -0600

    Update

--- C#/Fall 2022/Week 2/OOP Exercise5 Objects and Classes/Program.cs
+++ C#/Fall 2022/Week 2/OOP Exercise5 Objects and Classes/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using In_Class_Work;
+using System;
+namespace In_Class_Work
+{
+    class Program
+    {
+        static void Main()
+        {
+            int userinput = 0;
+            while (userinput != 5)
+            {
+                Console.WriteLine("Please input what you would like to do:");
+                userinput = int.Parse(Console.ReadLine());
+                if (userinput == 1)
+                {
+
+                }
+                else if (userinput == 2)
+                {
+
+                }
+                else if (userinput == 3)
+                {
+
+                }
+                else if (userinput == 4)
+                {
+
+                }
+                else
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
