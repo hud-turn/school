a8a0a45717316d8d3aba3eec4486dd1d296eb72c
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2022-10-13 16:47:48 -0600

    Changes

--- C#/Fall 2022/Week 2/OOP Exercise5 Objects and Classes/Team1.cs
+++ C#/Fall 2022/Week 2/OOP Exercise5 Objects and Classes/Team1.cs
@@ -8,12 +8,22 @@ namespace OOP_Exercise5_Objects_and_Classes
 {
     internal class Team1
     {
-        public Team1(string team1playername, Location team1playersloc)
+        public Team1(List <string> team1playername, List <Location> team1playersloc)
         {
             this.Team1PlayerName = team1playername;
             this.Team1PlayersLoc = team1playersloc;
         }
         public string Team1PlayerName { get; set; }
         public Location Team1PlayersLoc { get; set; }
+    }
+    public void PlayerNameandLoc()
+    {
+        public void ListAllMembers()
+        {
+            foreach (Team1 s in this.Team1PlayerName)
+            {
+                Console.WriteLine(s.Team1PlayerName);
+            }
+        }
     }
 }
