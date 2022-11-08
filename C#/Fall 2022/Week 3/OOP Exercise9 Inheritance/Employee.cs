0c4e56556d774b3839b9d7bd8cb0803c4a58a267
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2022-11-08 09:01:31 -0700

    Starting New Proj

---
+++ C#/Fall 2022/Week 3/OOP Exercise9 Inheritance/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Exercise5_Objects_and_Classes
+{
+    internal class Employee
+    {
+    }
+}
