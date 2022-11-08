986ae614451999f81dee18f352e5f685fd267d44
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2022-11-08 09:33:15 -0700

    Changes

--- C#/Fall 2022/Week 3/OOP Exercise9 Inheritance/Employee.cs
+++ C#/Fall 2022/Week 3/OOP Exercise9 Inheritance/Employee.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using OOP_Exercise9_Inheritance;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OOP_Exercise5_Objects_and_Classes
+namespace OOP_Exercise9_Objects_and_Classes
 {
     internal class Employee
     {
+        public Employee(bool teamaffil, string teamplayername, Location teamplayersloc, double hypotenuse)
+        {
+            this.TeamAffil = teamaffil;
+            this.TeamPlayerName = teamplayername;
+            this.TeamPlayersLoc = teamplayersloc;
+            this.Hypotenuse = hypotenuse;
+        }
+        public bool TeamAffil { get; set; }
+        public string TeamPlayerName { get; set; }
+        public Location TeamPlayersLoc { get; set; }
+        public double Hypotenuse { get; set; }
     }
 }
---
+++ C#/Fall 2022/Week 3/OOP Exercise9 Inheritance/Player.cs
@@ -0,0 +1,14 @@
+﻿using OOP_Exercise9_Inheritance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Exercise9_Objects_and_Classes
+public class Player : Employee
+{
+	public Player
+	{
+	}
+}
