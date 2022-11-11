08b3c4087babb28b5f650902115f1131e6b1962c
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2022-11-10 21:27:57 -0700

    Changes

--- C#/Fall 2022/Week 3/OOP Exercise9 Inheritance/Employee.cs
+++ C#/Fall 2022/Week 3/OOP Exercise9 Inheritance/Employee.cs
@@ -5,18 +5,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OOP_Exercise9_Objects_and_Classes
+namespace OOP_Exercise9_Inheritance
 {
     public class Employee
     {
-        public Employee(bool teamaffil, string teamplayername, Location teamplayersloc, double hypotenuse)
+        public Employee(string teamplayername, Location teamplayersloc, double hypotenuse, int teamaffil)
         {
             this.TeamAffil = teamaffil;
             this.TeamPlayerName = teamplayername;
             this.TeamPlayersLoc = teamplayersloc;
             this.Hypotenuse = hypotenuse;
         }
-        public bool TeamAffil { get; set; }
+        public int TeamAffil { get; set; }
         public string TeamPlayerName { get; set; }
         public Location TeamPlayersLoc { get; set; }
         public double Hypotenuse { get; set; }
--- C#/Fall 2022/Week 3/OOP Exercise9 Inheritance/Player.cs
+++ C#/Fall 2022/Week 3/OOP Exercise9 Inheritance/Player.cs
@@ -5,20 +5,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace OOP_Exercise9_Objects_and_Classes;
-internal class Player : Employee
+namespace OOP_Exercise9_Inheritance
 {
-	public Player(bool teamaffil, string teamplayername, Location teamplayersloc, double hypotenuse, int gametype) : base(teamaffil,teamplayername, teamplayersloc, hypotenuse)
-	{
-        this.TeamAffil = teamaffil;
-        this.TeamPlayerName = teamplayername;
-        this.TeamPlayersLoc = teamplayersloc;
-        this.Hypotenuse = hypotenuse;
-        this.GameType = gametype;
+    public class Player : Employee
+    {
+        public Player(int teamaffil, string teamplayername, Location teamplayersloc, double hypotenuse, int gametype) : base(teamplayername, teamplayersloc, hypotenuse, teamaffil)
+        {
+            this.TeamAffil = teamaffil;
+            this.TeamPlayerName = teamplayername;
+            this.TeamPlayersLoc = teamplayersloc;
+            this.Hypotenuse = hypotenuse;
+            this.GameType = gametype;
+        }
+        public int TeamAffil { get; set; }
+        public string TeamPlayerName { get; set; }
+        public Location TeamPlayersLoc { get; set; }
+        public double Hypotenuse { get; set; }
+        public int GameType { get; set; }
     }
-    public bool TeamAffil { get; set; }
-    public string TeamPlayerName { get; set; }
-    public Location TeamPlayersLoc { get; set; }
-    public double Hypotenuse { get; set; }
-    public int GameType { get; set; }
 }
