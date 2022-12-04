2c564801006aae4eeeb1024e4ec6359d0b95ead6
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2022-12-03 21:05:05 -0700

    Changes

---
+++ C#/Fall 2022/Week 3/OOP_Exercise10_Interfaces/Hotel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Exercise10_Interfaces
+{
+    internal class Hotel
+    {
+        public Hotel(double floornum, double roomnum, double unix_time_check_in, double unix_time_check_out )
+        {
+            this.FloorNum = floornum;
+            this.RoomNum = roomnum;
+            this.UnixCheckIn = unix_time_check_in;
+            this.UnixCheckOut = unix_time_check_out;
+        }
+        public double FloorNum { get; set; }
+        public double RoomNum { get; set; }
+        public double UnixCheckIn { get; set; }
+        public double UnixCheckOut { get; set; }
+    }
+}
