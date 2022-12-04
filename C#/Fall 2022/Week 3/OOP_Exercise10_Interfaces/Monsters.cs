1e229fa3e4d9a6dd03325e5847d3a83d2a460fd3
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2022-12-03 20:53:14 -0700

    New Program

---
+++ C#/Fall 2022/Week 3/OOP_Exercise10_Interfaces/Monsters.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Exercise10_Interfaces
+{
+    interface IMonster_Undead
+    {
+        void Undead();
+    }
+    interface IMonster_Drain_Health
+    {
+        void Drain_Health();
+    }
+    class Zombie : IMonster_Undead
+    {
+        public void Undead()
+        {
+            Console.WriteLine("Undead");
+        }
+    }
+    class Vampire : IMonster_Drain_Health, IMonster_Undead
+    {
+        public void Undead()
+        {
+            Console.WriteLine("Undead");
+        }
+        public void Drain_Health()
+        {
+            Console.WriteLine("Drain Health");
+        }
+    }
+    class Werewolf
+    {
+        public void Transform()
+        {
+            Console.WriteLine("Transform");
+        }
+    }
+    class Wyvern
+    {
+        public void Fire_Breath()
+        {
+            Console.WriteLine("Fire Breath");
+        }
+    }
+}
---
+++ C#/Fall 2022/Week 3/OOP_Exercise10_Interfaces/Program.cs
@@ -0,0 +1,17 @@
+﻿using OOP_Exercise8_Inheritance;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Security.Principal;
+
+namespace OOP_Exercise8_Inheritance
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+        }
+        
+    }
+}
