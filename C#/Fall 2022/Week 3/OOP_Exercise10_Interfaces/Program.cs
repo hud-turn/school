d80ebbe8dbf1f5a7f66eb105fb9b0fc49e92d0da
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2022-12-03 21:10:22 -0700

    Changes

--- C#/Fall 2022/Week 3/OOP_Exercise10_Interfaces/Program.cs
+++ C#/Fall 2022/Week 3/OOP_Exercise10_Interfaces/Program.cs
@@ -1,16 +1,19 @@
 ﻿using OOP_Exercise10_Interfaces;
-using OOP_Exercise8_Inheritance;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics.Metrics;
 using System.Security.Principal;
 
-namespace OOP_Exercise8_Inheritance
+namespace OOP_Exercise10_Interfaces
 {
     class Program
     {
         static void Main()
         {
+            List<Hotel> hotelList = new List<Hotel>();
+            Hotel example1 = new Hotel(1568574, 6542385, 263544, 6543621);
+            hotelList.Add(example1);
+
             Zombie zombie = new Zombie();
             zombie.Undead();
         }
