c2157cfed9885574ea86f4ac841bf9116502b3fc
Hudson Turner <30182083+hturner80631@users.noreply.github.com>
2022-12-10 12:48:53 -0700

    Changes

--- C#/Fall 2022/Week 3/OOP_Exercise_11_All_Concepts/Program.cs
+++ C#/Fall 2022/Week 3/OOP_Exercise_11_All_Concepts/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using OOP_Exercise_11_All_Concepts;
+using System;
+using System.Security.Principal;
+
+namespace OOP_Exercise_11_All_Concepts
+{
+    class OOP_Exercise_11_All_Concepts
+    {
+        static void Main(string[] args)
+        {
+            int intuserinput = 0;
+            while (intuserinput != 7)
+            {
+                Console.WriteLine("Welcome to the main menu!\nPlease input what function you would like to perform:\n1.Enter Course\n2.Enter Students\n3.Remove Students\n4.Remove Course\n5.Enter Student Grade\n6.Grade Analytics");
+                if (intuserinput == 1)
+                {
+
+                }
+                else if (intuserinput == 2)
+                {
+
+                }
+                else if (intuserinput == 3)
+                {
+
+                }
+                else if (intuserinput == 4)
+                {
+
+                }
+                else if (intuserinput == 5)
+                {
+
+                }
+                else if (intuserinput == 6)
+                {
+
+                }
+                else
+                {
+
+                }
+                Console.Clear();//clearing terminal
+            }
+        }
+    }
+}
